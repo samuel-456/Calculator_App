782032645dce0d497526eb302e8ac652a63db829
Samuel Nyong Idung <samcwth@gmail.com>
2022-09-30 11:28:43 -0700

    My First Commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun onDigit(view: View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        tvInput.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        tvInput.text = ""
+    }
+
+    }
+
